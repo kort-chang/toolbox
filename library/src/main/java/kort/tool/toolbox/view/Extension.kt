b869d4efaabe2e4289e6e180b1a11cb71e375ced
toanyone <toanyone19980217@gmail.com>
2019-09-18 16:16:18 +0800

    ✨ add view extension

---
+++ library/src/main/java/kort/tool/toolbox/view/Extension.kt
@@ -0,0 +1,24 @@
+package kort.tool.toolbox.view
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import java.lang.Exception
+
+/**
+ * Created by Kort on 2019/9/18.
+ */
+fun Context.obtainStyleAndRecycle(
+    attrs: AttributeSet,
+    declareStyleId: IntArray,
+    defStyleRes: Int = 0,
+    getStyle: TypedArray.() -> Unit
+) {
+    obtainStyledAttributes(attrs, declareStyleId, 0, defStyleRes).apply {
+        getStyle(this)
+        recycle()
+    }
+}
+
+const val private_get_message = "There is the private getter"
+fun privateGet(): Nothing = throw Exception(private_get_message)
