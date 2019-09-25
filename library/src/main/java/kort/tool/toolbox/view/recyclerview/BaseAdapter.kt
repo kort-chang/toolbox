f0c9baf7e89c4a46e5fd2ca2deed1770dba7ad1b
toanyone <toanyone19980217@gmail.com>
2019-09-25 14:06:05 +0800

    ✨ update BaseAdapter

--- library/src/main/java/kort/tool/toolbox/view/recyclerview/BaseAdapter.kt
+++ library/src/main/java/kort/tool/toolbox/view/recyclerview/BaseAdapter.kt
@@ -1,6 +1,5 @@
 package kort.tool.toolbox.view.recyclerview
 
-import android.view.ViewGroup
 import androidx.annotation.CallSuper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
@@ -8,12 +7,12 @@ import androidx.viewbinding.ViewBinding
 /**
  * Created by Kort on 2019/9/25.
  */
-abstract class BaseAdapter<T> : RecyclerView.Adapter<BaseViewHolder<T>>() {
+abstract class BaseAdapter<T, VH : BaseViewHolder<T>> : RecyclerView.Adapter<VH>() {
     open val currentList: MutableList<T> = emptyList<T>().toMutableList()
     override fun getItemCount(): Int = currentList.size
     open fun getItem(position: Int) = currentList[position]
     @CallSuper
-    override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
+    override fun onBindViewHolder(holder: VH, position: Int) {
         holder.bind(getItem(position))
     }
 }
