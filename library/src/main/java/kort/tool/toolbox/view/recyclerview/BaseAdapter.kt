057b734075b63dcf3d496948b162cdcff0f8deaa
toanyone <toanyone19980217@gmail.com>
2019-10-08 17:24:43 +0800

    ♻️ Clean the BaseAdapter

--- library/src/main/java/kort/tool/toolbox/view/recyclerview/BaseAdapter.kt
+++ library/src/main/java/kort/tool/toolbox/view/recyclerview/BaseAdapter.kt
@@ -6,20 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 /**
  * Created by Kort on 2019/9/25.
  */
-abstract class BaseAdapter<T, VH : BaseViewHolder> : RecyclerView.Adapter<VH>() {
-    open var focusAt: Int? = null
+abstract class BaseAdapter<T, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
     open var currentList: MutableList<T> = emptyList<T>().toMutableList()
     override fun getItemCount(): Int = currentList.size
     open fun getItem(position: Int): T = currentList[position]
-
-    protected open fun focusAt(position: Int, holder: BaseViewHolder) {
-        if (position == focusAt) {
-            holder.focus()
-            focusAt = null
-        }
-    }
-}
-
-abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    open fun focus() {}
 }
