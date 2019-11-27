a5686f4e83187017a1ad4b430671863a7bac894f
toanyone <toanyone19980217@gmail.com>
2019-11-28 00:38:48 +0800

    ✨ add type converter

--- library/src/main/java/kort/tool/toolbox/view/Extension.kt
+++ library/src/main/java/kort/tool/toolbox/view/Extension.kt
@@ -3,6 +3,7 @@ package kort.tool.toolbox.view
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
+import android.util.TypedValue
 import java.lang.Exception
 
 /**
@@ -23,3 +24,8 @@ fun Context.obtainStyleAndRecycle(
 const val private_get_message = "There is the private getter"
 fun privateGet(): Nothing = throw Exception(private_get_message)
 
+fun Context.dpToPx(dp: Int) =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), resources.displayMetrics)
+
+fun Context.dpToPx(dp: Float) =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
